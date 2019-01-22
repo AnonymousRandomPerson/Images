020c6df1801cbf82d69e1539f820a47092215831
Cheng Hann Gan <chenghanngan.us@gmail.com>
2019-01-22 16:34:36 -0500

    Fix link

--- snivy.sh
+++ snivy.sh
@@ -1,6 +1,6 @@
 # /bin/bash
 
-curl -X GET https://bulbapedia.bulbagarden.net/wiki/File:Juniper_Snivy.png > ~/Documents/lol.png
+curl -X GET https://cdn.bulbagarden.net/upload/8/88/Juniper_Snivy.png > ~/Documents/lol.png
 osascript -e '
 set pt to path to documents folder as string
 set img to pt & "lol.png"
